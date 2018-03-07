f075bbec8c8f85bd68bd7c6ab6237652c5306c41
ABardot <aabardot5@gmail.com>
2018-03-07 10:41:59 -0500

    Added the Director & Genre Class, properties for the class and the Navigation properties.

---
+++ MovieDirectorApp/MovieDirectorApp/Models/Director.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieDirectorApp.Models
+{
+    public class Director
+    {
+        [Key]
+        public int DirectorID { get; set; }
+
+        public string Name { get; set; }
+
+        // Navigation Prop
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
